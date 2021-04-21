7d72b89f82aa0a4039773fad820b9645e5c2a13a
navayuvansb <sb.navayuvan@gmail.com>
2021-04-21 11:36:43 +0530

    Define Director Model

--- movies/models.py
+++ movies/models.py
@@ -14,3 +14,21 @@ class Studio(models.Model):
 class Genre(models.Model):
     title = models.CharField(max_length=100, verbose_name="Genre Name")
     slug = models.SlugField(verbose_name="URL Param")
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, null=True)
+
+    phone_number = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+
+    website = models.URLField()
+
+    GENDER = (
+        ('1', "Male"),
+        ('2', "Female"),
+        ('3', "Unspecified"),
+    )
+    gender = models.CharField(max_length=2, choices=GENDER, default='1')
