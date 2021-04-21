5978e83e6b80faf79bd6513bdc74b6f7e631863b
navayuvansb <sb.navayuvan@gmail.com>
2021-04-21 11:31:54 +0530

    Add Genre Model

--- movies/models.py
+++ movies/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+
 class Studio(models.Model):
     title = models.CharField(max_length=100, verbose_name="Studio Name")
     prefix = models.CharField(max_length=10)
     website = models.URLField()
-    slug = models.SlugField(verbose_name='URL Param')
+    slug = models.SlugField(verbose_name="URL Param")
 
     def __str__(self):
         return self.title
-    
 
+
+class Genre(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Genre Name")
+    slug = models.SlugField(verbose_name="URL Param")
