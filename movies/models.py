df4cde6a76b0254e51878e8e44435654078474b5
navayuvansb <sb.navayuvan@gmail.com>
2021-04-21 11:30:03 +0530

    Define Studio model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Studio(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Studio Name")
+    prefix = models.CharField(max_length=10)
+    website = models.URLField()
+    slug = models.SlugField(verbose_name='URL Param')
+
+    def __str__(self):
+        return self.title
+    
+
