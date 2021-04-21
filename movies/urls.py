858650452b70ea4316ebc081d517c9a315bdc06d
navayuvansb <sb.navayuvan@gmail.com>
2021-04-21 12:51:11 +0530

    Add Movie List View

---
+++ movies/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.MovieListView.as_view(), name="movies"),
+]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Movie
 
-# Create your views here.
+
+class MovieListView(generic.ListView):
+    model = Movie
